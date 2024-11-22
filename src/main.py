682810154c4d419ac7f740d041f7f8e2a8b007c8
Ambrose <ambydavid1@gmail.com>
2024-11-22 10:58:02 +0100

    .

--- src/main.py
+++ src/main.py
@@ -1,6 +1,17 @@
 from PySide6.QtWidgets import QApplication
-from ui import TabWidgetApp
+import os
 import sys
+
+base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+ui_file_path = os.path.join(base_path, "ui.py")
+
+print("Looking for ui.py:", os.path.exists(ui_file_path))
+
+
+from ui import TabWidgetApp
+
+
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
